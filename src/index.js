4b451f8ac79fee0304da98a68a8c65742d30b72e
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-08 02:52:51 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Challenge 1
+
+// Add click eventListener to the button. Alert `It is 18 degrees`
+
+function showTemperature(event) {
+  event.preventDefault();
+
+
+
+
+
+// Challenge 2
+// When the button is clicked, replace button label by ‘18 degrees’
+
+  let monday = document.querySelector("button");
+  monday.innerHTML = "18 degrees";
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", showTemperature);
